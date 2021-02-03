1046c0533dafd38387bb1f753a7c5f6b1030c0cf
estevooliveira111 <63885372+estevooliveira111@users.noreply.github.com>
2021-02-03 14:37:47 -0300

    xxx

--- documento.php
+++ documento.php
@@ -11,31 +11,14 @@ $banco='gerador_de_volantes';
 
 
 
-$conexao = mysqli_connect('$server', '$usuario', '$senha', '$banco') or die('Erro ao conectar ao banco de dados');
+// $conexao = mysqli_connect("$server", "$usuario", "$senha", "$banco") or die('Erro ao conectar ao banco de dados');
+$conexao = mysqli_connect("$server", "$usuario", "$senha", "$banco");
+if (mysqli_connect_errno() != 0) {
+    echo "Erro ao acessar banco: " . mysqli_connect_error();
+    die();
+}
+
+header("Location: .$http/index.html");
 
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    
-
-    echo $nome,$senha;
-
-    ?>
-
-</body>
-</html>
-
-
-<php
-
-//header("Location: .$http/dados/index.html");
 ?>
