2c6ec979ae4a41eb8ebd2f0007bb1577570fb40b
estevooliveira111 <63885372+estevooliveira111@users.noreply.github.com>
2021-02-03 17:37:27 -0300

    voltar

---
+++ login/proc.php
@@ -0,0 +1,32 @@
+	
+	
+<?php
+
+if(isset($_POST['env'])){
+
+    ##SENHA PADRAO:
+    $default_password = 1234;
+    ## SENHA DIGITADA
+    $password = $_POST['password'];
+
+    ##VERIFICA SE A SENHA É A MESMA.
+    if( $password == $default_password ){
+
+        ##CRIA A SESSÃO
+        $_SESSION['password'] = $password;
+        ##REDIRECIONA
+        header("Location: pagina.php");
+
+    }else{
+        ##ERRO
+        echo "Senha incorreta.";
+
+    }
+
+
+
+}
+
+?>
+
+
