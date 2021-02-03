1e19d5ea669d6a271ac65aa21eab3c1e1195acd3
estevooliveira111 <63885372+estevooliveira111@users.noreply.github.com>
2021-02-03 13:31:18 -0300

    xxx

--- documento.php
+++ documento.php
@@ -1,41 +1,41 @@
 <?php
 
-include_once("conexao.php");
+$nome= $_POST['nome'];
+$email=$_POST['email'];
+$visor=$_POST['visor'];
 
-$nome = filter_input (INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-$email = filter_input (INPUT_POST, 'mail', FILTER_SANITIZE_STRING);
-$visor =filter_input (INPUT_POST, 'visor', FILTER_SANITIZE_STRING);
-
-
-
-$resultado_usuario = "INSERT INTO PESSOAS (NOME, email, NUMEROSESCOLHIDOS) VALUES ('$nome', '$email', '$visor')";
-
-$resultado_usuario = mysqli_query($conn,$conectar);
-
-
-$servidor='localhost';
+$server='localhost';
 $usuario='MASTERUSUARIO';
 $senha='37838691WWWww.';
-$database='';
-
-$conn= mysqli_connect($servidor, $usuario, $senha, $database);
-mysqli_select_db('gerador_de_volantes', $conn);
-
-
-     if(mysqli_connect_errno($conn)){
-         echo"Problemas";
-         die();
-    }
-
-    $pessoas = buscarpessoas($conn);
-
-        function buscar_pessoas($conn){
-        $sqlBuscar =   'SELECT * FROM PESSOAS';
-        $resultado = mysqli_query($conn,$sqlBuscar);
-    }
-     
-    $tarefas =array();
+$banco='gerador_de_volantes';
 
 
 
+//$conexao = mysqli_connect('$server', '$usuario', '$senha', '$banco') or die('Erro ao conectar ao banco de dados');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+
+    echo $nome,$senha;
+
+    ?>
+
+</body>
+</html>
+
+
+<php
+
+//header("Location: .$http/dados/index.html");
 ?>
