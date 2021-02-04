57cb052ccf1959f291e0cc576833e85d67aae1ad
estevooliveira111 <63885372+estevooliveira111@users.noreply.github.com>
2021-02-04 19:33:35 -0300

    xxxx

--- login/dados/documento.php
+++ login/dados/documento.php
@@ -1,59 +1,35 @@
-<?php
-	//$dados = $_FILES['arquivo'];
-	//var_dump($dados);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>nn</title>
+</head>
+<body>
 	
-	//if(!empty($_FILES['arquivo']['tmp_name'])){
-	//	$arquivo = new DomDocument();
-	//	$arquivo->load($_FILES['arquivo']['tmp_name']);
-	//	//var_dump($arquivo);
-	//	
-	//	$linhas = $arquivo->getElementsByTagName("Row");
-	//	//var_dump($linhas);
+</body>
+</html>
+
+<?php
+
+	// $arquivo = $_FILES["Arquivo"]["tmp_name"];
+
+	// $xml = simplexml_load_file($arquivo);
+
+	// foreach($xml->children() as $Worksheet) {
+	// 	echo $Worksheet->Row;
+	// }
 
 
-	$temporario = $_FILES["Arquivo"]["tmp_name"];
+	$dados = $_FILES['arquivo'];
+	var_dump($dados);
+	
+	if(!empty($_FILES['arquivo']['tmp_name'])){
+		$arquivo = new DomDocument();
+		$arquivo->load($_FILES['arquivo']['tmp_name']);
+		var_dump($arquivo);
+		
+		$linhas = $arquivo->getElementsByTagName("Row");
+		var_dump($linhas);
 
-	$xml = simplexml_load_file($temporario);
-
-
-	$xml = new DOMDocument();
-
-	$xml->load($_["Arquivo"]["tmp_name"]);
-
-		var_dump($xml);
-
-//		$linha = $xml->getElementsByTagName("Row");
-
-
-//		var_dump($linha);
-//
-//	foreach ( $linha as $linha){
-//			$n = $linha->getElementsByTagName("Data") ->item(0)->nodeValue;
-//			echo "Nome: $n";
-//		}
-
-
-
-$server='localhost';
-$usuario='MASTERUSUARIO';
-$senha='37838691WWWww.';
-$banco='gerador_de_volantes';
-
-    //$conexao = mysqli_connect("$server", "$usuario", "$senha", "$banco") or die('Erro ao conectar ao banco de dados');
-    $conexao = mysqli_connect("$server");
-    if (mysqli_connect_errno() != 0) {
-    //echo "Erro ao acessar banco: " . mysqli_connect_error();
-    die();
-
-    mysqli_select_db("$banco") or die( 'Erro na seleção do banco' );
-
-
-    $queryone ="SELECT NUMEROSDOXML, FROM NUMEROSDOXML";
-
-        $NOME =mysqli_query($conexao,$queryone);
-
-
-}
-
-
-	?>
+?>
