35aa45d81c16263f9830453bd85ab1d18d735676
estevooliveira111 <63885372+estevooliveira111@users.noreply.github.com>
2021-02-05 11:31:44 -0300

    xxx

--- dados/documento.php
+++ dados/documento.php
@@ -15,7 +15,8 @@
 if(!empty($_FILES['arquivo']['tmp_name'])){
 	$arquivo = new DomDocument();
 	$arquivo->load($_FILES['arquivo']['tmp_name']);
-	var_dump($arquivo);
+
+	//var_dump($arquivo);
 	
 	$linhas = $arquivo->getElementsByTagName("Row");
 	//var_dump($linhas);
@@ -24,17 +25,9 @@ if(!empty($_FILES['arquivo']['tmp_name'])){
 	
 	foreach($linhas as $linha){
 		if($primeira_linha == false){
-			$nome = $linha->getElementsByTagName("Data")->item(0)->nodeValue;
-			echo "Nome: $nome <br>";
 			
-			$email = $linha->getElementsByTagName("Data")->item(1)->nodeValue;
-			echo "E-mail: $email <br>";
-			
-			$niveis_acesso_id = $linha->getElementsByTagName("Data")->item(2)->nodeValue;
-			echo "Nivel de Acesso: $niveis_acesso_id <br>";
-			
-			
-			
+			$primeiro = $linha->getElementsByTagName("Cell")->item(2)->nodeValue;
+			echo "- $tercerio<br/>";
 		}
 		$primeira_linha = false;
 	}
