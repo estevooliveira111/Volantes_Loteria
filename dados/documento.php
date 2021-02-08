c9ededbb6dd8e47774b69859157227e5d60f97c8
Estevão Oliveira Nascimento <63885372+estevooliveira111@users.noreply.github.com>
2021-02-08 13:37:01 -0300

    xxx

--- dados/documento.php
+++ dados/documento.php
@@ -1,49 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>nn</title>
-</head>
-<body>
-	
-</body>
-</html>
+<?
 
-<?php
+$dados = $_FILES['arquivo'];
+$xml= simplexml_load_string($dados);
 
-	
-	$dia=date('d/m/Y');
-	$hors=date('H:i:s');
-
-	echo"$dia & $hora <br/><br/>";
-
-if(!empty($_FILES['arquivo']['tmp_name'])){
-	$arquivo = new DomDocument();
-	$arquivo->load($_FILES['arquivo']['tmp_name']);
-
-	//var_dump($arquivo);
-	
-	$linhas = $arquivo->getElementsByTagName("Row");
-	//var_dump($linhas);
-	
-	$primeira_linha = true;
-	
-	foreach($linhas as $linha){
-		if($primeira_linha == false){
-			
-
-			
-
-			$primeiro= $linha->getElementsByTagName("Data")->item(2)->nodeValue;
-			echo  "$primeiro";
-			
-
-		}
-		$primeira_linha = false;
-	}
-}
-
+	foreach($xml->Row->Row as $Row){
+		echo $Row->Cell;
+	};
 
 
 ?>
