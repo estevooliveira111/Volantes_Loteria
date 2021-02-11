6a36a84b01f5b4f4170d90a8ccf328d913dc8d0f
Estevão Oliveira Nascimento <63885372+estevooliveira111@users.noreply.github.com>
2021-02-11 13:41:03 -0300

    xxxx

--- Volantes/index.php
+++ Volantes/index.php
@@ -35,58 +35,51 @@ li{
 
 <body>
 
-    <?php
-
-        include 'conexao.php';
-
-    ?>
 
     <h1 class="Title">Volantes Gerados</h1>
 
     <div class="base">
         <?php
 
-    if (!$conn) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-    }else{
-    echo "<br>Sucesso: foi feita uma conexão adequada com o MySQL! O banco de dados my_db é ótimo.<br><br>" . PHP_EOL;
-    echo "Informações do host: " . mysqli_get_host_info($conn) . PHP_EOL;
-    echo "<br><br>";
 
-    mysqli_select_db($dbname, $con);
-    // cria a instrução SQL que vai selecionar os dados
-    $query = sprintf("SELECT nome, email, Numerosescolhidos, Datadiahora FROM Pessoas");
-    // executa a query
-    $dados = mysqli_query($query, $con) or die(mysql_error());
-    // transforma os dados em um array
-    $linha = mysqli_fetch_assoc($dados);
-    // calcula quantos dados retornaram
-    $total = mysqli_num_rows($dados);
+include 'conexao.php';
 
+
+$result_usuario = "SELECT * FROM Pessoas ORDER BY id DESC";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+//Verificar se encontrou resultado na tabela "usuarios"
+if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)){
+	while($row_usuario = mysqli_fetch_assoc($resultado_usuario)){
+		echo $row_usuario['nome'] . "<br>";
+            $query=sprintf("SELECT nome, email, Numerosescolhidos, Datadiahora FROM Pessoas");
+            $dados = mysqli_query($query, $con) or die(mysql_error());
+            mysqli_select_db($dbname, $con);
+                // cria a instrução SQL que vai selecionar os dados
+                $query = sprintf("SELECT nome, email, Numerosescolhidos, Datadiahora FROM Pessoas");
+                // executa a query
+                $dados = mysqli_query($query, $con) or die(mysql_error());
+                // transforma os dados em um array
+                $linha = mysqli_fetch_assoc($dados);
+                // calcula quantos dados retornaram
+                $total = mysqli_num_rows($dados);
+    
+    }
 }
 
-?>
-        </div>
 
-        <div class="base">
-
-        <?php
 	// se o número de resultados for maior que zero, mostra os dados
 	if($total > 0) {
-		// inicia o loop que vai mostrar todos os dados
 		do {
 ?>
-			<p><?=$linha['nome']?> / <?=$linha['']?></p>
+			<li><?=$linha['nome']?> / <?=$linha['Numerosescolhidos
+            ']?></li>
 <?php
 		// finaliza o loop que vai mostrar os dados
-		}while($linha = mysqli_fetch_assoc($dados));
+		}while($linha = mysql_fetch_assoc($dados));
 	// fim do if
 	}
 ?>
-
 
         </div>
 
