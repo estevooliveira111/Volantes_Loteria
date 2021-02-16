f023fbb0cb1f581c890661324dc3a4e28b031daf
Estevão Oliveira Nascimento <63885372+estevooliveira111@users.noreply.github.com>
2021-02-16 09:42:36 -0300

    xx

---
+++ dados/enviardados/dados.php
@@ -0,0 +1,9 @@
+<?php
+
+// method="post"
+// <input type="hidden" name="enviou" value="1">
+// <input id="file" type="file" name="arquivo" id="Arquivo"><br><br>
+// <input id="linkII" name="enviar" value="Enviar
+
+
+?>
